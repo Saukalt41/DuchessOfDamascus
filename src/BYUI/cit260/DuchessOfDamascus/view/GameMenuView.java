4eee7b51e59013b1e91fb239a6f4110967ec6ce0
Blanco <Blanco@Blanco-PC.PK5001Z>
2014-06-04 18:42:02 -0600

    Fied conflict with location class

--- src/BYUI/cit260/DuchessOfDamascus/view/GameMenuView.java
+++ src/BYUI/cit260/DuchessOfDamascus/view/GameMenuView.java
@@ -6,6 +6,10 @@
 
 package BYUI.cit260.DuchessOfDamascus.view;
 
+import BYUI.cit260.DuchessOfDamascus.control.ProgramControl;
+import DuchessOfDamascus.DuchessOfDamascus;
+import java.util.Scanner;
+
 /**
  *
  * @author Bernardo Blanco
@@ -14,11 +18,82 @@ class GameMenuView {
     
     private static final String GameMenu = "\n"
             + "\n-------------------------------"
-            + "\n | Game Menu |"
+            + "\n| Game Menu                   |"
             + "\n-------------------------------"
+            + "\nS - Save Game                 |"
+            + "\nI - Inventory                 |"
+            + "\nM - Map                       |"
+            + "\nH - Help                      |"
+            + "\nE - Exit                      |"
+            + "\n-------------------------------";
 
-    void displayMenu() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    void displayMenu(){
+        //Not yet finished
+        char selection = ' ';
+        do {
+            
+            System.out.println(GameMenu);
+            
+            String input = this.getInput();
+            selection = input.charAt(0);
+            
+            this.doAction(selection);
+        } while (selection != 'E');
+        }
+    
+    public String getInput() {
+        boolean valid = false; //This will tell us if the name has been acquired
+        String menuChoice = null;
+        Scanner keyboard = new Scanner(System.in); // This allows us to use the keyboard to input something in the game
+
+        while (!valid) { // while a name has not yet been entered
+
+            // Ask user for the player's name
+            System.out.println("Choose a menu option:");
+
+            // Receive name from the keyboard and trim blanks
+            menuChoice = keyboard.nextLine();
+            menuChoice = menuChoice.trim();
+            menuChoice = menuChoice.toUpperCase();
+
+            if (menuChoice.toUpperCase().equals("E")) { //Quitting ?
+                return null;
+            }
+            // Name is invalid (non-blank or less than two characters)
+            if (menuChoice == "S" || menuChoice == "I" || menuChoice == "M" || menuChoice == "H" || menuChoice == "E") {
+                //display error
+                System.out.println(menuChoice + " is not a valid choice."
+                        + "\nPlease choose S, I, M, H or E.");
+            } else {
+                valid = true; //Valid name entered
+            }
+            
+        }
+        return menuChoice; //return the name    
     }
+    public void doAction(char choice) {
+        
+        switch (choice){
+            case 'S':
+                GameMenuView gameMenu = new GameMenuView();
+                gameMenu.displayMenu();
+                break;
+            case 'I':
+                HelpMenuView helpMenu = new HelpMenuView();
+                helpMenu.displayMenu();
+                break;
+            case 'M':
+                ProgramControl.saveGame(DuchessOfDamascus.getCurrentGame());
+                break;
+            case 'H':
+                ProgramControl.loadGame(DuchessOfDamascus.getCurrentGame());
+                break;
+            case 'E':
+                return;
+            default:
+                System.out.println("\n*** Invalid selection *** Try again");
+                break;
+        }
+    }    
     
 }
