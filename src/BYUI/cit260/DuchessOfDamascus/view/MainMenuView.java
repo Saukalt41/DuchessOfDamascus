f025e1022e6fecf021f5374eabc154f66261d7d6
Saukalt41 <sco13002@byui.edu>
2014-06-18 16:10:20 -0600

    Merge origin/master
    Conflicts:
    	src/BYUI/cit260/DuchessOfDamascus/control/ProgramControl.java
    	src/BYUI/cit260/DuchessOfDamascus/view/StartProgramView.java

--- src/BYUI/cit260/DuchessOfDamascus/view/MainMenuView.java
+++ src/BYUI/cit260/DuchessOfDamascus/view/MainMenuView.java
@@ -15,9 +15,10 @@ import java.util.Scanner;
  *
  * @author Bernardo Blanco
  */
-public class MainMenuView {
-    
-    private static final String Menu = "\n"
+public class MainMenuView extends View {
+     
+    public MainMenuView() {
+     super("\n"
             + "\n--------------------------------------------"
             + "\n| Main Menu                                |"
             + "\n--------------------------------------------"
@@ -26,52 +27,9 @@ public class MainMenuView {
             + "\nS - Save Game                              |"
             + "\nL - Load Game                              |"
             + "\nE - Exit                                   |"
-            + "\n--------------------------------------------";
-    
-    public void displayMenu(){
-        //Not yet finished
-        char selection = ' ';
-        do {
-            
-            System.out.println(Menu);
-            
-            String input = this.getInput();
-            selection = input.charAt(0);
-            
-            this.doAction(selection);
-        } while (selection != 'E');
-        }
-    
-    public String getInput() {
-        boolean valid = false; //This will tell us if the name has been acquired
-        String menuChoice = null;
-        Scanner keyboard = new Scanner(System.in); // This allows us to use the keyboard to input something in the game
-
-        while (!valid) { // while a name has not yet been entered
-
-            // Ask user for the player's name
-            System.out.println("Choose a menu option:");
-
-            // Receive name from the keyboard and trim blanks
-            menuChoice = keyboard.nextLine();
-            menuChoice = menuChoice.trim();
-            menuChoice = menuChoice.toUpperCase();
-
-            if (menuChoice.toUpperCase().equals("E")) { //Quitting ?
-                return null;
-            }
-            // Name is invalid (non-blank or less than two characters)
-            if (menuChoice == "G" || menuChoice == "H" || menuChoice == "S" || menuChoice == "E") {
-                //display error
-                System.out.println(menuChoice + " is not a valid choice."
-                        + "\nPlease choose G, H, S, or E.");
-            } else {
-                valid = true; //Valid name entered
-            }
-            
-        }
-        return menuChoice; //return the name    
+            + "\n--------------------------------------------");
     }
+    
     public void doAction(char choice) {
         
         switch (choice){
@@ -103,6 +61,11 @@ public class MainMenuView {
         //display the game menu
         GameMenuView gameMenu = new GameMenuView();
         gameMenu.displayMenu();
+    }
+
+    @Override
+    public void doAction(String value) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
     
 }
