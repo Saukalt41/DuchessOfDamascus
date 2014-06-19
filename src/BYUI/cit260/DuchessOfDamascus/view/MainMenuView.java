053ba6ddf7df0a871fdebd2997f52b3edcc5c6c9
Saukalt41 <sco13002@byui.edu>
2014-06-19 16:17:58 -0600

    Merge origin/master
    Conflicts:
    	src/BYUI/cit260/DuchessOfDamascus/control/ProgramControl.java
    	src/BYUI/cit260/DuchessOfDamascus/view/StartProgramView.java

--- src/BYUI/cit260/DuchessOfDamascus/view/MainMenuView.java
+++ src/BYUI/cit260/DuchessOfDamascus/view/MainMenuView.java
@@ -3,7 +3,6 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package BYUI.cit260.DuchessOfDamascus.view;
 
 import BYUI.cit260.DuchessOfDamascus.control.GameControl;
@@ -16,23 +15,24 @@ import java.util.Scanner;
  * @author Bernardo Blanco
  */
 public class MainMenuView extends View {
-     
+
     public MainMenuView() {
-     super("\n"
-            + "\n--------------------------------------------"
-            + "\n| Main Menu                                |"
-            + "\n--------------------------------------------"
-            + "\nG - Start game                             |"
-            + "\nH - Get help on how to play the game       |"
-            + "\nS - Save Game                              |"
-            + "\nL - Load Game                              |"
-            + "\nE - Exit                                   |"
-            + "\n--------------------------------------------");
+        super("\n"
+                + "\n--------------------------------------------"
+                + "\n| Main Menu                                |"
+                + "\n--------------------------------------------"
+                + "\nG - Start game                             |"
+                + "\nH - Get help on how to play the game       |"
+                + "\nS - Save Game                              |"
+                + "\nL - Load Game                              |"
+                + "\nE - Exit                                   |"
+                + "\n--------------------------------------------");
     }
+
     @Override
-    public void doAction(char choice) {
-        
-        switch (choice){
+    public void doAction(String choice) {
+
+        switch (choice.trim().toUpperCase().charAt(0)) {
             case 'G':
                 this.startNewGame();
                 break;
@@ -57,21 +57,10 @@ public class MainMenuView extends View {
     private void startNewGame() {
         //create a new game
         GameControl.createNewGame(DuchessOfDamascus.getPlayer());
-        
+
         //display the game menu
         GameMenuView gameMenu = new GameMenuView();
         gameMenu.displayMenu();
     }
 
-    @Override
-    public void doAction(String value) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-
-    
-
-   
-    
 }
-
-
